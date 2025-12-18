0d3ee3dceae5f2a926db3d86f3b6f37dae47f250
ZenJO204 <s6703052411023@email.kmutnb.ac.th>
2025-12-18 11:57:22 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-   <table border="1">
-      <tr>
-        <th>Product</th>
-        <th>Price</th>
-      </tr>
-      <tr>
-        <td>React</td>
-        <td>320</td>
-      </tr>
-      <tr>
-        <td>React Navtice</td>
-        <td>320</td>
-      </tr>
-   </table>
-  );
+  const divStyle = {
+    color: 'red',
+    backgroundColor: 'powderblue',
+    fontSize: 'large',
+    padding: '3px'
+  };
+  return <div style={divStyle}>Hello React</div>
 }
 
 export default App;
