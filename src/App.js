18a5d1d67bc51bf8bbe36088da8fe22d7e003182
ZenJO <s6703052411023@email.kmutnb.ac.th>
2026-01-29 11:03:15 +0700

    Context&State

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { userContext } from "./context";
-import Content from './context-content'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
+import React from "react";
 function App() {
+    let [user, setUser] = React.useState('')
     return (
-        <userContext.Provider value={'DeMoNiX'}>
-            <Content/>
+        <userContext.Provider value={[user, setUser]}>
+            <Header2/>
+            <Content2/>
         </userContext.Provider>
     )
 }
