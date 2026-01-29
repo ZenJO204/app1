2a095544d8ccfb47514db08a2cce5183f65de1e5
DeMoNiX204 <chatchanan918@gmail.com>
2026-01-29 10:13:09 +0700

    context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from 'react'
-import MessageBox from './state-func'
-export default function App() {
-return <MessageBox/>
+import { userContext } from "./context";
+import Content from './context-content'
+function App() {
+    return (
+        <userContext.Provider value={'DeMoNiX'}>
+            <Content/>
+        </userContext.Provider>
+    )
 }
+export default App;
 
