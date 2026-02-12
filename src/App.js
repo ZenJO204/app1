1f6f35b1ddf14d6da123006e529bf42291a82a35
ZenJO <s6703052411023@email.kmutnb.ac.th>
2026-02-12 10:02:24 +0700

    NavLink

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
-import { userContext } from "./context";
-import Header2 from './context-header2'
-import Content2 from './context-content2'
+import Router2 from './router2'
 import React from "react";
-function App() {
-    let [user, setUser] = React.useState('')
-    return (
-        <userContext.Provider value={[user, setUser]}>
-            <Header2/>
-            <Content2/>
-        </userContext.Provider>
-    )
+export default function App () {
+    return <Router2/>
 }
-export default App;
 
